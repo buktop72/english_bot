9e279f2a11ed624bd229f7af2745d639a838e01b
buktop72 <73024265+buktop72@users.noreply.github.com>
2025-07-06 19:13:25 +0300

    Update config.py
    fix config

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 #HF_API_TOKEN = os.getenv("HF_API_TOKEN")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-WEBHOOK_URL=""
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
